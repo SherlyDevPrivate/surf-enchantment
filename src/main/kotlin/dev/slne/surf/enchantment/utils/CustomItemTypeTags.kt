4bbb237420102a959e8c08a0c89d112fa480c3a0
twisti <denizgebauer@web.de>
2025-07-13 23:52:07 +0200

    Refactored `CustomItemTypeTags` to simplify constructor and improve readability.

--- src/main/kotlin/dev/slne/surf/enchantment/utils/CustomItemTypeTags.kt
+++ src/main/kotlin/dev/slne/surf/enchantment/utils/CustomItemTypeTags.kt
@@ -2,75 +2,63 @@
 
 package dev.slne.surf.enchantment.utils
 
+import dev.slne.surf.surfapi.core.api.messages.adventure.key
 import dev.slne.surf.surfapi.core.api.util.objectSetOf
 import io.papermc.paper.registry.RegistryKey
 import io.papermc.paper.registry.keys.ItemTypeKeys
 import io.papermc.paper.registry.tag.TagKey
 import io.papermc.paper.tag.TagEntry
+import io.papermc.paper.tag.TagEntry.valueEntry
 import it.unimi.dsi.fastutil.objects.ObjectSet
 import net.kyori.adventure.key.Key
 import org.bukkit.inventory.ItemType
 
-enum class CustomItemTypeTags(
-    val tagKey: TagKey<ItemType>,
-    val tagEntries: ObjectSet<TagEntry<ItemType>>
-) {
-
+enum class CustomItemTypeTags(key: Key, private vararg val tags: TagEntry<ItemType>) {
     WOODEN_TOOLS_KEY(
-        TagKey.create(RegistryKey.ITEM, Key.key("surf", "wooden_tools")),
-        objectSetOf(
-            TagEntry.valueEntry(ItemTypeKeys.WOODEN_PICKAXE),
-            TagEntry.valueEntry(ItemTypeKeys.WOODEN_AXE),
-            TagEntry.valueEntry(ItemTypeKeys.WOODEN_SHOVEL),
-            TagEntry.valueEntry(ItemTypeKeys.WOODEN_HOE),
-        )
+        key("surf", "wooden_tools"),
+        valueEntry(ItemTypeKeys.WOODEN_PICKAXE),
+        valueEntry(ItemTypeKeys.WOODEN_AXE),
+        valueEntry(ItemTypeKeys.WOODEN_SHOVEL),
+        valueEntry(ItemTypeKeys.WOODEN_HOE),
     ),
 
     IRON_TOOLS_KEY(
-        TagKey.create(RegistryKey.ITEM, Key.key("surf", "iron_tools")),
-        objectSetOf(
-            TagEntry.valueEntry(ItemTypeKeys.IRON_PICKAXE),
-            TagEntry.valueEntry(ItemTypeKeys.IRON_AXE),
-            TagEntry.valueEntry(ItemTypeKeys.IRON_SHOVEL),
-            TagEntry.valueEntry(ItemTypeKeys.IRON_HOE),
-        )
+        key("surf", "iron_tools"),
+        valueEntry(ItemTypeKeys.IRON_PICKAXE),
+        valueEntry(ItemTypeKeys.IRON_AXE),
+        valueEntry(ItemTypeKeys.IRON_SHOVEL),
+        valueEntry(ItemTypeKeys.IRON_HOE),
     ),
 
     GOLD_TOOLS_KEY(
-        TagKey.create(RegistryKey.ITEM, Key.key("surf", "gold_tools")),
-        objectSetOf(
-            TagEntry.valueEntry(ItemTypeKeys.GOLDEN_PICKAXE),
-            TagEntry.valueEntry(ItemTypeKeys.GOLDEN_AXE),
-            TagEntry.valueEntry(ItemTypeKeys.GOLDEN_SHOVEL),
-            TagEntry.valueEntry(ItemTypeKeys.GOLDEN_HOE),
-        )
+        key("surf", "gold_tools"),
+        valueEntry(ItemTypeKeys.GOLDEN_PICKAXE),
+        valueEntry(ItemTypeKeys.GOLDEN_AXE),
+        valueEntry(ItemTypeKeys.GOLDEN_SHOVEL),
+        valueEntry(ItemTypeKeys.GOLDEN_HOE),
     ),
 
     DIAMOND_TOOLS_KEY(
-        TagKey.create(RegistryKey.ITEM, Key.key("surf", "diamond_tools")),
-        objectSetOf(
-            TagEntry.valueEntry(ItemTypeKeys.DIAMOND_PICKAXE),
-            TagEntry.valueEntry(ItemTypeKeys.DIAMOND_AXE),
-            TagEntry.valueEntry(ItemTypeKeys.DIAMOND_SHOVEL),
-            TagEntry.valueEntry(ItemTypeKeys.DIAMOND_HOE),
-        )
+        key("surf", "diamond_tools"),
+        valueEntry(ItemTypeKeys.DIAMOND_PICKAXE),
+        valueEntry(ItemTypeKeys.DIAMOND_AXE),
+        valueEntry(ItemTypeKeys.DIAMOND_SHOVEL),
+        valueEntry(ItemTypeKeys.DIAMOND_HOE),
     ),
 
     NETHERITE_TOOLS_KEY(
-        TagKey.create(RegistryKey.ITEM, Key.key("surf", "netherite_tools")),
-        objectSetOf(
-            TagEntry.valueEntry(ItemTypeKeys.NETHERITE_PICKAXE),
-            TagEntry.valueEntry(ItemTypeKeys.NETHERITE_AXE),
-            TagEntry.valueEntry(ItemTypeKeys.NETHERITE_SHOVEL),
-            TagEntry.valueEntry(ItemTypeKeys.NETHERITE_HOE),
-        )
+        key("surf", "netherite_tools"),
+        valueEntry(ItemTypeKeys.NETHERITE_PICKAXE),
+        valueEntry(ItemTypeKeys.NETHERITE_AXE),
+        valueEntry(ItemTypeKeys.NETHERITE_SHOVEL),
+        valueEntry(ItemTypeKeys.NETHERITE_HOE),
     ),
 
     TOOLS_KEY(
-        TagKey.create(RegistryKey.ITEM, Key.key("surf", "tools")),
-        objectSetOf(
-            DIAMOND_TOOLS_KEY.tagEntries
-        )
-    )
+        key("surf", "tools"),
+        *DIAMOND_TOOLS_KEY.tags,
+    );
 
+    val tagKey = TagKey.create(RegistryKey.ITEM, key)
+    val tagEntries: ObjectSet<TagEntry<ItemType>> = objectSetOf(*tags)
 }
