d5f4c25cda48eca9533a0d5e59948059e9b34c22
Ammo <ammo@slne.dev>
2025-07-13 23:42:18 +0200

    Remove unused TelekinesisListener from SilentNightEnchantment

--- src/main/kotlin/dev/slne/surf/enchantment/enchantments/SilentNightEnchantment.kt
+++ src/main/kotlin/dev/slne/surf/enchantment/enchantments/SilentNightEnchantment.kt
@@ -9,9 +9,6 @@ import dev.slne.surf.surfapi.core.api.messages.adventure.key
 import dev.slne.surf.surfapi.core.api.util.objectSetOf
 import io.papermc.paper.registry.keys.EnchantmentKeys
 import io.papermc.paper.registry.keys.tags.EnchantmentTagKeys
-import org.bukkit.event.EventHandler
-import org.bukkit.event.Listener
-import org.bukkit.event.block.BlockDropItemEvent
 import org.bukkit.inventory.ItemRarity
 
 object SilentNightEnchantment : CustomEnchantment(
@@ -34,20 +31,4 @@ object SilentNightEnchantment : CustomEnchantment(
 
     )
 ) {
-    object TelekinesisListener : Listener {
-        @EventHandler
-        fun onBlockBreak(event: BlockDropItemEvent) {
-            val player = event.player
-
-            event.items.forEach { drop ->
-                val notAdded = player.inventory.addItem(drop.itemStack)
-
-                notAdded.forEach { key, item ->
-                    event.block.location.world.dropItem(event.block.location, item)
-                }
-            }
-
-            event.items.clear()
-        }
-    }
 }
