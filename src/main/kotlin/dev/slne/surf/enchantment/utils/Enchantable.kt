854f35983fa4e8aecc9f3ed7f1863a866a04312b
Jo_field <150937665+JoField08@users.noreply.github.com>
2026-01-05 15:59:25 +0100

    Add Roman numeral conversion for enchantment levels in Enchantable.kt

--- src/main/kotlin/dev/slne/surf/enchantment/utils/Enchantable.kt
+++ src/main/kotlin/dev/slne/surf/enchantment/utils/Enchantable.kt
@@ -1,6 +1,7 @@
 package dev.slne.surf.enchantment.utils
 
 import dev.slne.surf.surfapi.bukkit.api.builder.LoreBuilder
+import dev.slne.surf.surfapi.core.api.util.int2IntMapOf
 import net.kyori.adventure.key.Key
 import net.kyori.adventure.text.Component
 import org.jetbrains.annotations.Range
@@ -17,9 +18,37 @@ interface Enchantable {
         line {
             append(displayName.color(rarity.color))
             appendSpace()
-            append(Component.text(level, rarity.color))
+            text(levelToRoman(level), rarity.color)
         }
         description(level)
     }.build()
 
+    private fun levelToRoman(level: Int): String {
+        val numerals = listOf(
+            1000 to "M",
+            900 to "CM",
+            500 to "D",
+            400 to "CD",
+            100 to "C",
+            90 to "XC",
+            50 to "L",
+            40 to "XL",
+            10 to "X",
+            9 to "IX",
+            5 to "V",
+            4 to "IV",
+            1 to "I"
+        )
+
+        var n = level
+        val result = StringBuilder()
+
+        for ((value, roman) in numerals) {
+            val count = n / value
+            repeat(count) { result.append(roman) }
+            n %= value
+        }
+
+        return result.toString()
+    }
 }
