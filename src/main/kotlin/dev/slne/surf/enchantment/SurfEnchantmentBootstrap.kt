f822f64f03a301cd3cd5bd3a293618be34b4fda2
Jo_field <150937665+JoField08@users.noreply.github.com>
2026-01-07 18:06:40 +0100

    Refactor SurfEnchantmentBootstrap to streamline event handler registration

--- src/main/kotlin/dev/slne/surf/enchantment/SurfEnchantmentBootstrap.kt
+++ src/main/kotlin/dev/slne/surf/enchantment/SurfEnchantmentBootstrap.kt
@@ -7,14 +7,10 @@ import dev.slne.surf.enchantment.bootstrap.tagPostFlattenHandler
 import dev.slne.surf.enchantment.bootstrap.tagPreFlattenHandler
 import io.papermc.paper.plugin.bootstrap.BootstrapContext
 import io.papermc.paper.plugin.bootstrap.PluginBootstrap
-import io.papermc.paper.plugin.lifecycle.event.types.LifecycleEvents
-import io.papermc.paper.registry.event.RegistryEvents
 
 class SurfEnchantmentBootstrap : PluginBootstrap {
     override fun bootstrap(context: BootstrapContext) {
         with(context.lifecycleManager) {
-
-
             registerEventHandler(tagPreFlattenHandler())
             registerEventHandler(tagPostFlattenHandler())
             registerEventHandler(enchantmentComposeHandler())
