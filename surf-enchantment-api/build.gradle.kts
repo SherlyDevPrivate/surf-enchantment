9273d4fcf0853778e9a6a798804749fe9070b87d
Ammo <ammo@slne.dev>
2026-03-12 20:20:49 +0100

    feat: add Maven publication configuration to build.gradle.kts

--- surf-enchantment-api/build.gradle.kts
+++ surf-enchantment-api/build.gradle.kts
@@ -1,3 +1,11 @@
 plugins {
     id("dev.slne.surf.surfapi.gradle.paper-raw")
 }
+
+publishing {
+    publications {
+        create<MavenPublication>("maven") {
+            from(components["java"])
+        }
+    }
+}
