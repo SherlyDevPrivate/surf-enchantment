b6832589762a4700b64431b637ce24b46341cf2b
Ammo <ammo@slne.dev>
2026-03-12 20:25:41 +0100

    feat: update Gradle wrapper to version 9.3.0 and configure Maven publishing repository

--- surf-enchantment-api/build.gradle.kts
+++ surf-enchantment-api/build.gradle.kts
@@ -1,11 +1,11 @@
+import dev.slne.surf.surfapi.gradle.util.slneReleases
+
 plugins {
     id("dev.slne.surf.surfapi.gradle.paper-raw")
 }
 
 publishing {
-    publications {
-        create<MavenPublication>("maven") {
-            from(components["java"])
-        }
+    repositories {
+        slneReleases()
     }
 }
