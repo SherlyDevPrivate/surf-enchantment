0fcf4b0ee1b47432e2a1d2130919a0adc363638d
TheBjoRedCraft <thebjoredcraft@gmail.com>
2026-05-01 13:28:54 +0200

    feat: add listener to prevent illegal enchantments in anvil crafting

--- surf-enchantment-paper/src/main/kotlin/dev/slne/surf/enchantment/paper/SurfEnchantment.kt
+++ surf-enchantment-paper/src/main/kotlin/dev/slne/surf/enchantment/paper/SurfEnchantment.kt
@@ -1,9 +1,11 @@
 package dev.slne.surf.enchantment.paper
 
 import com.github.shynixn.mccoroutine.folia.SuspendingJavaPlugin
+import dev.slne.surf.api.paper.event.register
 import dev.slne.surf.api.paper.packet.SurfPaperPacketApi
 import dev.slne.surf.enchantment.paper.commands.surfEnchantmentCommand
 import dev.slne.surf.enchantment.paper.enchantment.enchantmentManagerImpl
+import dev.slne.surf.enchantment.paper.listener.IllegalAnvilEnchantmentsListener
 import dev.slne.surf.enchantment.paper.lore.SurfEnchantmentPacketLoreHandler
 import org.bukkit.plugin.java.JavaPlugin
 
@@ -15,10 +17,15 @@ class SurfEnchantment : SuspendingJavaPlugin() {
 
     override suspend fun onEnableAsync() {
         enchantmentManagerImpl.registerEnchantmentListeners()
-        SurfPaperPacketApi.INSTANCE.registerPacketLoreListenerGlobal(this, SurfEnchantmentPacketLoreHandler)
+        SurfPaperPacketApi.INSTANCE.registerPacketLoreListenerGlobal(
+            this,
+            SurfEnchantmentPacketLoreHandler
+        )
         enchantmentManagerImpl.startEnchantmentJobs()
 
         surfEnchantmentCommand()
+
+        IllegalAnvilEnchantmentsListener.register()
     }
 
     override suspend fun onDisableAsync() {
---
+++ surf-enchantment-paper/src/main/kotlin/dev/slne/surf/enchantment/paper/listener/IllegalAnvilEnchantmentsListener.kt
@@ -0,0 +1,42 @@
+package dev.slne.surf.enchantment.paper.listener
+
+import org.bukkit.enchantments.Enchantment
+import org.bukkit.event.EventHandler
+import org.bukkit.event.Listener
+import org.bukkit.event.inventory.PrepareAnvilEvent
+import org.bukkit.inventory.ItemStack
+import org.bukkit.inventory.meta.EnchantmentStorageMeta
+
+object IllegalAnvilEnchantmentsListener : Listener {
+    @EventHandler
+    fun onAnvilPrepare(event: PrepareAnvilEvent) {
+        val left = event.inventory.firstItem ?: return
+        val right = event.inventory.secondItem ?: return
+
+        if (!isBook(left) || !isBook(right)) {
+            return
+        }
+
+        val leftEnchants = getStoredEnchants(left)
+        val rightEnchants = getStoredEnchants(right)
+
+        for ((enchant, levelLeft) in leftEnchants) {
+            val levelRight = rightEnchants[enchant] ?: continue
+
+            val resultLevel =
+                if (levelLeft == levelRight) levelLeft + 1 else maxOf(levelLeft, levelRight)
+
+            if (resultLevel > enchant.maxLevel) {
+                event.result = null
+                return
+            }
+        }
+    }
+
+    private fun isBook(item: ItemStack) = item.itemMeta is EnchantmentStorageMeta
+
+    private fun getStoredEnchants(item: ItemStack): Map<Enchantment, Int> {
+        val meta = item.itemMeta as? EnchantmentStorageMeta ?: return emptyMap()
+        return meta.storedEnchants
+    }
+}
